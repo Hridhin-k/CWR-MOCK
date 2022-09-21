07e40d71c12ca2612295a5e7f2f78875b6e27b0e
hridhin <hridhin@bititude.com>
2022-09-21 11:55:48 +0530

    multiple routing outlets implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { AllchecksComponent } from './allchecks/allchecks.component';
+import { CommunicationComponent } from './communication/communication.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { EvidenceComponent } from './evidence/evidence.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ReportsComponent } from './reports/reports.component';
 import { VerifiersComponent } from './verifiers/verifiers.component';
 
-const routes: Routes = [{ path: 'Dashboard', component: DashboardComponent }, { path: 'Employees', component: ProfileComponent }
+const routes: Routes = [{ path: 'Dashboard', component: DashboardComponent }, {
+  path: 'Employees', component: ProfileComponent,
+  children: [{ path: '', component: AllchecksComponent }, { path: 'allchecks', component: AllchecksComponent }, { path: 'communications', component: CommunicationComponent }]
+}
   , { path: 'Reports', component: ReportsComponent },
 { path: 'Verifiers', component: VerifiersComponent },
 { path: 'Admin', component: AdminComponent },
 { path: 'evidence', component: EvidenceComponent },
+
 ];
 
 @NgModule({
